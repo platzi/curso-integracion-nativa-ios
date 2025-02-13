ce57b8510bbea71e90efc11907d4ea20d5cfc6bc
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2025-02-13 16:55:52 -0400

    Base code for sensors coremotion class

--- NativeComponents-iOS/NativeComponents-iOS/Views/modulo_4/DemoSensoresView.swift
+++ NativeComponents-iOS/NativeComponents-iOS/Views/modulo_4/DemoSensoresView.swift
@@ -6,24 +6,13 @@
 import SwiftUI
 
 struct DemoSensoresView: View {
-    @StateObject private var motionManager = MotionManager()
     
     var body: some View {
         VStack {
-            Text("Acelerómetro")
-                .font(.largeTitle)
-            Text("X: \(motionManager.accelerationX, specifier: "%.2f")")
-            Text("Y: \(motionManager.accelerationY, specifier: "%.2f")")
-            Text("Z: \(motionManager.accelerationZ, specifier: "%.2f")")
+            
             
         }
         .padding()
-        .onAppear {
-            motionManager.startUpdates()
-        }
-        .onDisappear {
-            motionManager.stopUpdates()
-        }
         
     }
 }
