c6c10a8696fbbdfccbd9895bb30ac2b119d8059d
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2025-02-13 17:21:20 -0400

    Base code for audio video class

--- NativeComponents-iOS/NativeComponents-iOS/Views/modulo_3/audio_video/DemoAudioView.swift
+++ NativeComponents-iOS/NativeComponents-iOS/Views/modulo_3/audio_video/DemoAudioView.swift
@@ -6,19 +6,11 @@
 import SwiftUI
 
 struct DemoAudioView: View {
-    @StateObject var audioPlayerViewModel = AudioPlayerViewModel()
     
     var body: some View {
         VStack {
-            Button(action: {
-                audioPlayerViewModel.playOrPause()
-            }) {
-                Image(systemName: audioPlayerViewModel.isPlaying ? "pause.circle" : "play.circle")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
 
-       }
+        }
     }
 }
 
