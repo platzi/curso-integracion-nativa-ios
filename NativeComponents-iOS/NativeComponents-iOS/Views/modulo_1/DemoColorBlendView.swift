0a462305578a246cdccaf2700907b2d4308099f3
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2025-02-13 19:11:06 -0400

    Base code for color blending class

--- NativeComponents-iOS/NativeComponents-iOS/Views/modulo_1/DemoColorBlendView.swift
+++ NativeComponents-iOS/NativeComponents-iOS/Views/modulo_1/DemoColorBlendView.swift
@@ -6,19 +6,12 @@
 import SwiftUI
 
 struct DemoColorBlendView: View {
-    @State private var colorOne: Color = Color.green
-    @State private var colorTwo: Color = Color.blue
     
     var body: some View {
         VStack {
-            TimelineView(.animation) { timeline in
-                let time = timeline.date.timeIntervalSince1970
-                let mix = (sin(time) + 1)/2
-                RoundedRectangle(cornerRadius: 8)
-                    .fill(colorOne.mix(with: colorTwo, by: mix))
-                    .frame(width: 400, height: 800)
-                
-            }
+
+            
+            
         }
         .navigationTitle("Color Blend Examples")
     }
