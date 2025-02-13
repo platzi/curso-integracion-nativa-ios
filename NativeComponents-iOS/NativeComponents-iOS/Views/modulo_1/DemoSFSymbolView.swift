c041e8d5c61d375a53112333eb14416fbbd082b6
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2025-02-13 19:18:19 -0400

    Base code for sf symbols updates class

--- NativeComponents-iOS/NativeComponents-iOS/Views/modulo_1/DemoSFSymbolView.swift
+++ NativeComponents-iOS/NativeComponents-iOS/Views/modulo_1/DemoSFSymbolView.swift
@@ -18,41 +18,25 @@ struct DemoSFSymbolView: View {
                     Toggle("Toggle", isOn: $showSmartReplace.animation())
                     Text("Con reemplazo mágico:")
                         .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                    Image(systemName: showSmartReplace ? "pencil.slash": "pencil")
-                        .contentTransition(.symbolEffect)
-                        .font(.largeTitle)
-                        .padding(.vertical, 16)
+                    
+                    
                     
                     Text("Sin reemplazo mágico:")
                         .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                    Image(systemName: showSmartReplace ? "pencil.slash": "pencil")
-                        .font(.largeTitle)
-                        .padding(.vertical, 16)
+                   
+                    
                 }
                 
                 Group {
                     GroupBox {
                         Toggle("Meneo/Movimiento", isOn: $wiggle)
-                        Image(systemName: "key.2.on.ring.fill")
-                            .symbolEffect(.wiggle, value: wiggle)
-                            .font(.largeTitle)
-                            .padding(.vertical, 16)
+                        
+                        
                     }
                     GroupBox {
                         Toggle("Meneo/Movimiento", isOn: $wiggle)
                         HStack {
-                            Image(systemName: "key.2.on.ring.fill")
-                                .symbolEffect(.wiggle.up, value: wiggle)
-                                .font(.largeTitle)
-                                .padding(.vertical, 16)
-                            Image(systemName: "key.2.on.ring.fill")
-                                .symbolEffect(.wiggle.byLayer, value: wiggle)
-                                .font(.largeTitle)
-                                .padding(.vertical, 16)
-                            Image(systemName: "key.2.on.ring.fill")
-                                .symbolEffect(.wiggle.backward, value: wiggle)
-                                .font(.largeTitle)
-                                .padding(.vertical, 16)
+                            
                             
                         }
                     }
@@ -60,19 +44,14 @@ struct DemoSFSymbolView: View {
                    
                     GroupBox {
                         Toggle("Rotación", isOn: $rotate)
-                        Image(systemName: "steeringwheel.and.hands")
-                            .symbolEffect(.rotate, value: rotate)
-                            .font(.largeTitle)
-                            .padding(.vertical, 16)
+                        
+                        
                     }
                     Group {
                         GroupBox {
                             Toggle("Efecto respira", isOn: $breathe)
-                            Image(systemName: "lungs.fill")
-                                .foregroundStyle(Color.pink)
-                                .symbolEffect(.breathe, options: .repeat(.continuous), value: breathe)
-                                .font(.largeTitle)
-                                .padding(.vertical, 16)
+                            
+                            
                         }
                     }
                 }
