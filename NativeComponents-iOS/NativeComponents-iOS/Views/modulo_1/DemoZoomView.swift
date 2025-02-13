12952386b2ea9471ef7631f75c4ad620be54a172
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2025-02-13 19:22:55 -0400

    Base code for zoom transitions class

--- NativeComponents-iOS/NativeComponents-iOS/Views/modulo_1/DemoZoomView.swift
+++ NativeComponents-iOS/NativeComponents-iOS/Views/modulo_1/DemoZoomView.swift
@@ -12,24 +12,7 @@ struct DemoZoomView: View {
 
     var body: some View {
         VStack(spacing: 10.0) {
-            NavigationLink {
-                VStack {
-                    Text("Contenido")
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
-                }
-                .navigationTransition(
-                    .zoom(
-                        sourceID: transitionID,
-                        in: zoomExampleNamespace
-                    )
-                )
-            } label: {
-                Text("Abrir pantalla")
-                    .padding(.all, 10)
-                    .background(Color.green)
-                    .clipShape(RoundedRectangle(cornerRadius: 8))
-            }
-            .matchedTransitionSource(id: transitionID, in: zoomExampleNamespace)
+            
         }
         .navigationTitle("Zoom Transitions")
     }
