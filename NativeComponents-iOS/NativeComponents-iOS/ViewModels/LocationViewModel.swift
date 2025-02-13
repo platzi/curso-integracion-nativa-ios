8911dd0615cd096ac525ed10548fd167de0f01e2
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2025-02-13 15:38:33 -0400

    Base code for geolocalization class

--- NativeComponents-iOS/NativeComponents-iOS/ViewModels/LocationViewModel.swift
+++ NativeComponents-iOS/NativeComponents-iOS/ViewModels/LocationViewModel.swift
@@ -3,27 +3,6 @@
 //  NativeComponents-iOS
 //
 
-import Foundation
-import CoreLocation
-
-class LocationViewModel: NSObject, CLLocationManagerDelegate, ObservableObject {
-    @Published var location: CLLocationCoordinate2D?
-    private let locationManager = CLLocationManager()
-    
-    override init() {
-        super.init()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        
-        DispatchQueue.main.async {
-            self.location = location.coordinate
-        }
-    }
+class LocationViewModel {
     
 }
