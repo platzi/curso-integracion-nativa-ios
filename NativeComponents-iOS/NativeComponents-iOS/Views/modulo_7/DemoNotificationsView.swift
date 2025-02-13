5df44c090bc1f200188feb7c93208323fd71d835
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2025-02-13 15:34:12 -0400

    Base code for notifications class

--- NativeComponents-iOS/NativeComponents-iOS/Views/modulo_7/DemoNotificationsView.swift
+++ NativeComponents-iOS/NativeComponents-iOS/Views/modulo_7/DemoNotificationsView.swift
@@ -8,27 +8,7 @@ import SwiftUI
 struct DemoNotificationsView: View {
     var body: some View {
         VStack {
-            Text("Notificaciones en SwiftUI")
-                .font(.largeTitle)
-                .padding()
-            Button(action:
-                    NotificationManager.shared.requestPermission
-            ) {
-                Text("Solicitar permiso")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color.blue)
-                    .cornerRadius(8)
-            }
-            Button(action:
-                    NotificationManager.shared.scheduleNotification
-            ) {
-                Text("Programar notificación")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color.blue)
-                    .cornerRadius(8)
-            }
+            
         }
     }
 }
